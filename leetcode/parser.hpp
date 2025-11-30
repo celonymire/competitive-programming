9d0c76e47406e1f6f4b3a69013d3a2cb9c4b1774
Sam Huang <samestimable2016@gmail.com>
2025-11-30 09:35:34 -0800

    Modernize LeetCode parser with C++ idioms

--- leetcode/parser.hpp
+++ leetcode/parser.hpp
@@ -1,3 +1,6 @@
+// LeetCode keeps up to date with latest compilers, therefore we can adapt to
+// newer standards and features
+
 #pragma once
 
 #include <bits/stdc++.h>
@@ -9,7 +12,8 @@ struct TreeNode {
   TreeNode *right = nullptr;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
   ~TreeNode() {
     delete left;
     delete right;
@@ -21,106 +25,92 @@ struct ListNode {
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
-  ~ListNode() {
-    delete next;
-  }
+  ~ListNode() { delete next; }
 };
 
 template <typename T>
-struct is_bitset : std::false_type {};
-template <std::size_t N>
-struct is_bitset<bitset<N>> : std::true_type {};
+concept has_to_string = requires(T t) {
+  { t.to_string() } -> same_as<std::string>;
+};
 template <typename T>
-concept tuple_like = requires { typename tuple_size<T>::type; };
+concept tuple_like = requires(T t) { typename tuple_size<T>::type; };
 
 template <typename T>
-void print_impl(FILE *f, const T &val, bool write_newline) {
+void print_impl(ostream &os, const T &val, bool write_newline) {
   if constexpr (same_as<T, char>)
-    fprintf(f, "\"%c\"", val);
-  else if constexpr (same_as<T, int>)
-    fprintf(f, "%d", val);
-  else if constexpr (same_as<T, float>)
-    fprintf(f, "%.f", val);
-  else if constexpr (same_as<T, double>)
-    fprintf(f, "%.18f", val);
-  else if constexpr (same_as<T, long long>)
-    fprintf(f, "%lld", val);
-  else if constexpr (same_as<T, unsigned int>)
-    fprintf(f, "%u", val);
-  else if constexpr (same_as<T, unsigned long long>)
-    fprintf(f, "%llu", val);
-  else if constexpr (same_as<T, char *> || same_as<T, const char *>)
-    fprintf(f, "%s", val);
+    os << '"' << val << '"';
   else if constexpr (same_as<T, bool>)
-    fprintf(f, "%s", val ? "true" : "false");
+    os << (val ? "true" : "false");
   else if constexpr (same_as<T, string>)
-    fprintf(f, "\"%s\"", val.data());
-  else if constexpr (is_bitset<T>::value)
-    fprintf(f, "\"%s\"", val.to_string().data());
+    os << '"' << val << '"';
+  else if constexpr (is_arithmetic_v<T>)
+    os << val;
+  else if constexpr (has_to_string<T>)
+    os << '"' << val.to_string() << '"';
   else if constexpr (same_as<T, TreeNode *>) {
     queue<TreeNode *> q;
-    fprintf(f, "[");
+    os << '[';
     if (val) {
       q.push(val);
-      fprintf(f, "%d", val->val);
+      os << val->val;
     }
     while (!q.empty()) {
       auto cur = q.front();
       q.pop();
       if (cur->left) {
         q.push(cur->left);
-        fprintf(f, ",%d", cur->left->val);
+        os << ',' << cur->left->val;
       } else
-        fprintf(f, ",null");
+        os << ",null";
       if (cur->right) {
         q.push(cur->right);
-        fprintf(f, ",%d", cur->right->val);
+        os << ',' << cur->right->val;
       } else
-        fprintf(f, ",null");
+        os << ",null";
     }
-    fprintf(f, "]");
+    os << ']';
   } else if constexpr (tuple_like<T>) {
-    fprintf(f, "{");
+    os << '{';
     apply(
         [&, c = 0](auto &&...args) mutable {
-          ((fprintf(f, c++ ? "," : ""), print_impl(f, args, false)), ...);
+          ((c++ ? os << ',' : os, print_impl(os, args, false)), ...);
         },
         val);
-    fprintf(f, "}");
+    os << '}';
   } else if constexpr (same_as<T, ListNode *>) {
     auto cur = val;
-    fprintf(f, "[");
+    os << '[';
     while (cur) {
       if (cur != val)
-        fprintf(f, ",");
-      fprintf(f, "%d", cur->val);
+        os << ',';
+      os << cur->val;
       cur = cur->next;
     }
-    fprintf(f, "]");
+    os << ']';
   } else if constexpr (ranges::range<T>) {
-    fprintf(f, "[");
+    os << '[';
     for (auto it = val.begin(); it != val.end(); it++) {
       if (it != val.begin())
-        fprintf(f, ",");
-      print_impl(f, *it, false);
+        os << ',';
+      print_impl(os, *it, false);
     }
-    fprintf(f, "]");
+    os << ']';
   } else
     static_assert(false, "printing for type not supported");
   if (write_newline)
-    fprintf(f, "\n");
+    os << '\n';
 }
 
 #define CONCAT_IMPL(x, y) x##y
 #define CONCAT(x, y) CONCAT_IMPL(x, y)
 #define NUM_ARGS_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
 #define NUM_ARGS(...) NUM_ARGS_IMPL(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
-#define DBG_VAL(x)                  \
-  [&]() {                           \
-    auto val = x;                   \
-    fprintf(stderr, "[%s = ", #x);  \
-    print_impl(stderr, val, false); \
-    fprintf(stderr, "] ");          \
+#define DBG_VAL(x)                                                             \
+  [&]() {                                                                      \
+    auto val = x;                                                              \
+    cerr << "[" << #x << " = ";                                                \
+    print_impl(cerr, val, false);                                              \
+    cerr << "] ";                                                              \
   }()
 #define DBG_1(x) DBG_VAL(x)
 #define DBG_2(x, ...) DBG_VAL(x), DBG_1(__VA_ARGS__)
@@ -132,45 +122,47 @@ void print_impl(FILE *f, const T &val, bool write_newline) {
 #define DBG_8(x, ...) DBG_VAL(x), DBG_7(__VA_ARGS__)
 #define DBG_9(x, ...) DBG_VAL(x), DBG_8(__VA_ARGS__)
 #define DBG_10(x, ...) DBG_VAL(x), DBG_9(__VA_ARGS__)
-#define dbg(...) CONCAT(DBG_, NUM_ARGS(__VA_ARGS__))(__VA_ARGS__), fprintf(stderr, "\n");
+#define dbg(...) CONCAT(DBG_, NUM_ARGS(__VA_ARGS__))(__VA_ARGS__), cerr << '\n';
 // supports up to 10 arguments debugging at one time
 
-template <typename T>
-T parse() {
+template <typename T> T parse(istream &is = cin) {
   T ans;
-  if constexpr (same_as<T, char>)
-    scanf("\"%c\"", &ans);
-  else if constexpr (same_as<T, int>)
-    scanf("%d", &ans);
-  else if constexpr (same_as<T, long long>)
-    scanf("%lld", &ans);
-  else if constexpr (same_as<T, unsigned int>)
-    scanf("%u", &ans);
-  else if constexpr (same_as<T, unsigned long long>)
-    scanf("%llu", &ans);
-  else if constexpr (same_as<T, float>)
-    scanf("%f", &ans);
-  else if constexpr (same_as<T, double>)
-    scanf("%lf", &ans);
-  else if constexpr (same_as<T, string>) {
-    char *buf;
-    scanf(" \"%m[^\"]\"", &buf);
-    ans = buf;
-    free(buf);
+  char ch;
+  if constexpr (same_as<T, char>) {
+    is >> ch; // skip '"'
+    is >> ans;
+    is >> ch; // skip '"'
+  } else if constexpr (same_as<T, bool>) {
+    string s;
+    is >> s;
+    ans = (s == "true");
+  } else if constexpr (same_as<T, string>) {
+    is >> ch; // skip '"'
+    getline(is, ans, '"');
+  } else if constexpr (is_arithmetic_v<T>) {
+    is >> ans;
   } else if constexpr (same_as<T, TreeNode *>) {
-    bool right = true;
-    auto dummy = new TreeNode{};
-    queue<TreeNode *> q;
-    q.push(dummy);
-    scanf(" %*c");
-    if (int c = getchar(); c != ']') {
-      ungetc(c, stdin);
-      ungetc('[', stdin);
-      while (!q.empty() && getchar() != ']') {
-        char *buf;
-        scanf(" %m[^],]", &buf);
-        if (strcmp(buf, "null") != 0) {
-          auto new_node = new TreeNode{atoi(buf)};
+    is >> ch; // skip '['
+    is >> ws;
+    if (is.peek() == ']') {
+      is >> ch;
+      ans = nullptr;
+    } else {
+      bool right = true;
+      auto dummy = new TreeNode{};
+      queue<TreeNode *> q;
+      q.push(dummy);
+
+      while (is && is.peek() != ']') {
+        is >> ws;
+        string token;
+        char c;
+        while (is && (c = is.peek()) != ',' && c != ']') {
+          token += c;
+          is.get();
+        }
+        if (token != "null") {
+          auto new_node = new TreeNode{stoi(token)};
           if (right)
             q.front()->right = new_node;
           else
@@ -180,35 +172,47 @@ T parse() {
         if (right)
           q.pop();
         right = !right;
-        free(buf);
+
+        if (is.peek() == ',')
+          is >> ch;
       }
+      is >> ch; // skip ']'
+      ans = dummy->right;
+      dummy->right = nullptr;
+      delete dummy;
     }
-    ans = dummy->right;
   } else if constexpr (same_as<T, ListNode *>) {
     auto dummy = new ListNode{};
     auto cur = dummy;
-    scanf(" %*c");
-    if (int c = getchar(); c != ']') {
-      ungetc(c, stdin);
+    is >> ch; // skip '['
+    is >> ws;
+    if (is.peek() != ']') {
       while (true) {
-        cur->next = new ListNode{parse<int>()};
+        cur->next = new ListNode{parse<int>(is)};
         cur = cur->next;
-        if (getchar() == ']')
+        is >> ws;
+        if (is.peek() == ']')
           break;
+        is >> ch; // skip ','
       }
     }
+    is >> ch; // skip ']'
     ans = dummy->next;
+    dummy->next = nullptr;
     delete dummy;
   } else if constexpr (ranges::range<T>) {
-    scanf(" %*c");
-    if (int c = getchar(); c != ']') {
-      ungetc(c, stdin);
+    is >> ch; // skip '['
+    is >> ws;
+    if (is.peek() != ']') {
       while (true) {
-        ans.emplace_back(parse<typename T::value_type>());
-        if (getchar() == ']')
+        ans.emplace_back(parse<typename T::value_type>(is));
+        is >> ws;
+        if (is.peek() == ']')
           break;
+        is >> ch; // skip ','
       }
     }
+    is >> ch; // skip ']'
   } else
     static_assert(false, "parsing for type not supported");
   return ans;
@@ -217,17 +221,18 @@ T parse() {
 template <typename Solution, typename R, typename... Ts>
 void run(R (Solution::*fn)(Ts...)) {
   tuple<Solution, decay_t<Ts>...> args;
-  get<0>(args) = Solution{};
   [&]<size_t... Idx>(index_sequence<Idx...>) {
-    (((get<Idx + 1>(args) = parse<decay_t<Ts>>()), scanf("%*c")), ...);
+    (((get<Idx + 1>(args) = parse<decay_t<Ts>>())), ...);
   }(index_sequence_for<Ts...>{});
   if constexpr (same_as<R, void>) {
     apply(fn, args);
     []<size_t... Idx>(auto &&args, index_sequence<Idx...>) {
-      ((printf("#%lld: ", Idx + 1), print_impl(stdout, get<Idx + 1>(args), true)), ...);
+      ((cout << "#" << (Idx + 1) << ": ",
+        print_impl(cout, get<Idx + 1>(args), true)),
+       ...);
     }(args, index_sequence_for<Ts...>{});
   } else {
     auto res = apply(fn, args);
-    print_impl(stdout, res, true);
+    print_impl(cout, res, true);
   }
 }
