ab62ea074dbd5f0382eedfd9d02a8ef1cf951892
celonymire <celonymire@users.noreply.github.com>
2026-05-07 22:55:28 +0000

    deploy: 57d4e63267c14514b1251334586dbba91f3aa865

--- search/all_e.js
+++ search/all_e.js
@@ -5,5 +5,6 @@ var searchData=
   ['pow_2',['pow',['../d1/d8d/classmatrix.html#a302ba8fbc179816ef1faa6e64c2255eb',1,'matrix::pow()'],['../df/d1e/classmodint.html#a7e34f5f8a9ad0d5671c8b770cf7e9cf2',1,'modint::pow(long long p) const'],['../df/d1e/classmodint.html#a5f91f67572c041a399d37ed83478f4aa',1,'modint::pow(const modint&lt; M2 &gt; &amp;p) const']]],
   ['power_5fmonoid_3',['power_monoid',['../d2/d47/structpower__monoid.html',1,'']]],
   ['power_5fmonoid_2ehpp_4',['power_monoid.hpp',['../dc/d33/power__monoid_8hpp.html',1,'']]],
-  ['primes_5',['primes',['../d8/d31/structsieve.html#ac806e0938f901cdcc624ab17f33084e3',1,'sieve']]]
+  ['prime_5ffactors_5',['prime_factors',['../d8/d31/structsieve.html#ac2539e62e80b47a2a66b7af3b0cf3042',1,'sieve']]],
+  ['primes_6',['primes',['../d8/d31/structsieve.html#ac806e0938f901cdcc624ab17f33084e3',1,'sieve']]]
 ];
